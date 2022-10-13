970309008ff7f4fbebe31aa8e69e9af1d1ca188a
motain1 <jacksontfry@gmail.com>
2022-10-13 20:08:58 +1100

    adding results

--- components/Header.js
+++ components/Header.js
@@ -6,11 +6,10 @@ import {
   SearchIcon,
   UserIcon,
 } from "@heroicons/react/outline";
-
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
 
-function Header() {
+const Header = () => {
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
@@ -29,6 +28,6 @@ function Header() {
       />
     </header>
   );
-}
+};
 
 export default Header;
---
+++ components/Results.js
@@ -0,0 +1,14 @@
+import ThumbNail from "./ThumbNail";
+import FlipMove from "react-flip-move";
+
+function Results({results}) {
+  return (
+    <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4'>
+      {results.map((result) => (
+        <ThumbNail key={result.id} result={result} />
+      ))}
+    </FlipMove>
+  );
+}
+
+export default Results;
